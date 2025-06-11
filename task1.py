0778d1ea7a5b8b4be0a031979212a8a36d5f8f44
Іван Гнетило <iwan00002007@gmail.com>
2025-06-11 09:38:32 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,21 @@
+import random
 
+def limited_attempts_code():
+    secret = random.randint(1, 20)
+    attempts = 3
+    while attempts > 0:
+        print(f"Очікується введення... (залишилось {attempts} спроб)")
+        guess = input(f"Вгадайте число від 1 до 20: ")  
+        if not guess.isdigit(): 
+            print("Будь ласка, введіть число!")
+            continue  
+        
+        guess = int(guess)  
+        if guess == secret:
+            print("Вітаю, ви вгадали!")
+            return
+        attempts -= 1
+    
+    print(f"Ви програли! Загадане число: {secret}")
+
+limited_attempts_code()
